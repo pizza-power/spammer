2073a4030c7942877bf1ddeeb0b0a0bd986ed675
Matt <6135659+pizza-power@users.noreply.github.com>
2021-12-21 11:52:14 -0500

    Update mailer.py to enable message attachments
    enable message attachments

--- mailer.py
+++ mailer.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 
 import smtplib
-
+from os.path import basename
+from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from email.utils import COMMASPACE, formatdate
 
+# open a text file containing one email address per line
 with open("emails.txt") as f:
     emails = f.readlines()
 
@@ -14,6 +17,8 @@ IP = ""  # put the IP of the server here
 PORT = 25  # put port number here
 
 mailserver = smtplib.SMTP(IP, PORT)
+# if attaching a file, put it here
+files = []  # Example: "/home/user/contacts.txt"
 
 try:
     mailserver.ehlo()
@@ -21,11 +26,21 @@ try:
     for email in emails:
         try:
             msg = MIMEMultipart()
-            msg['From'] = "example@example.com"
-            msg['To'] = email
-            msg['Subject'] = "click here"
-            message = "put your message here"
+            msg["From"] = "example@example.com"  # put your email here
+            msg["To"] = email
+            msg["Subject"] = "subject"  # put the subject line here
+            message = "message"  # put the message here
             msg.attach(MIMEText(message))
+
+            for f in files or []:
+                with open(f, "rb") as fil:
+                    part = MIMEApplication(fil.read(), Name=basename(f))
+                # After the file is closed
+                part[
+                    "Content-Disposition"
+                ] = 'attachment; filename="%s"' % basename(f)
+                msg.attach(part)
+
             mailserver.sendmail("example@example.com", email, msg.as_string())
             print("email sent to %s" % email)
         except:
